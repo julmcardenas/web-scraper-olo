14fc85db363c1eaf352b194a4632694885460c3f
luisarevalo21 <arevaloluis89@yahoo.com>
2024-08-16 20:52:09 -0700

    sent a request to our backend and received it on the frontend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+
+// Enable CORS
+app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with your frontend URL
+    methods: "GET,POST", // Allow specific HTTP methods
+    allowedHeaders: "Content-Type,Authorization", // Allow specific headers
+  })
+);
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
@@ -10,6 +21,9 @@ app.use(express.static(path.join(__dirname, "client/build")));
 // Handle API requests here
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the server!" });
+});
+app.get("/api/products", (req, res) => {
+  res.json({ message: "projects are" });
 });
 
 // All other requests point to React's index.html
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+import axios from "axios";
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get("http://localhost:5001/api/products");
+      console.log(response.data);
+    }
+    getData();
+  }, []);
 
   return (
     <>

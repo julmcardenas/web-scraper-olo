8ec08025a82c9e0af4c9689be1d1af9be13570a6
Dharshan Bellan <dbellan1291@gmail.com>
2024-08-16 20:56:51 -0700

    made basic web scraper api

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
@@ -12,10 +12,15 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello from the server!" });
 });
 
+
 // All other requests point to React's index.html
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "client/build/index.html"));
 // });
+
+const apiRoutes = require("./routes/scrape");
+app.use("/scrape", apiRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
